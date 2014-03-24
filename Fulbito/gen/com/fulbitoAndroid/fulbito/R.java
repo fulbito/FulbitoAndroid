da3122f1bffa0fa94f86def6d31bc1e0ddc9a018
mac87-git <carabajal.m.a@gmail.com>
2014-03-23 22:04:04 -0300

    Se agrego el activity de inicio de la apliación que contiene el splash de la misma y que contendrá la lógica de inicialización de la app

--- Fulbito/gen/com/fulbitoAndroid/fulbito/R.java
+++ Fulbito/gen/com/fulbitoAndroid/fulbito/R.java
@@ -55,17 +55,20 @@ public final class R {
         public static final int txtVwRegistrar=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_mod_usuario=0x7f030001;
+        public static final int activity_login_usuario=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_mod_usuario=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int mod_usuario=0x7f070001;
+        public static final int login_usuario=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int mod_usuario=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_login_usuario=0x7f05000c;
         public static final int title_activity_mod_usuario=0x7f050003;
         public static final int txtBotonIngresar=0x7f050008;
         public static final int txtBotonRegistrarme=0x7f05000b;
--- Fulbito/src/com/fulbitoAndroid/fulbito/MainActivity.java
+++ Fulbito/src/com/fulbitoAndroid/fulbito/MainActivity.java
@@ -1,117 +1,40 @@
 /* ----------------------------------------------------------------------------- 
 Nombre: 		MainActivity
-Descripción:	Clase que contiene la lógica de login y registración de la aplicación
+Descripción:	Clase que contiene lanza el proceso de inicialización de la 
+				aplicación mientras muestra un splash de la aplicación
 
 Log de modificaciones:
 
 Fecha		Autor		Descripción
-22/03/2014	MAC			Creación
+23/03/2014	MAC			Creación
 ----------------------------------------------------------------------------- */
 
 package com.fulbitoAndroid.fulbito;
-import java.io.IOException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import com.fulbitoAndroid.admUsuario.ModUsuarioActivity;
+import com.fulbitoAndroid.admUsuario.LoginUsuarioActivity;
 import com.fulbitoAndroid.fulbito.R;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	//Atributos privados de la clase
-	private static final String URL_REGISTRAR_USR = "http://www.fulbitoweb.com.ar/procesaRegistracion.php?alias=%1$s&email=%2$s&password=%3$s&registrar=true";
-	private EditText edtTextAlias;
-	private EditText edtTextCorreo;
-	private EditText edtTextContrasena;
-	private EditText edtTextConfirmarContrasena;
 
-	//Metodos publicos
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        //Obtenemos los controles EditText
-        edtTextAlias = (EditText) findViewById(R.id.edtTxtAlias);
-    	edtTextCorreo = (EditText) findViewById(R.id.edtTxtEmailRegistrar);
-    	edtTextContrasena = (EditText) findViewById(R.id.edtTxtContrasenaRegistrar);
-    	edtTextConfirmarContrasena = (EditText) findViewById(R.id.edtTxtConfirmarContrasena);
-    	
-    	//Seteamos el tipo de fuente a los TextView txtVwLogin y txtVwRegistrar
-    	Typeface typeFace = Typeface.createFromAsset(getAssets(), "Helvetica-Black-SemiBold.ttf");
-        
-        TextView txtVwLogin = (TextView) findViewById(R.id.txtVwLogin);        
-        txtVwLogin.setTypeface(typeFace);
-        
-        TextView txtVwRegistrar = (TextView) findViewById(R.id.txtVwRegistrar);       
-        txtVwRegistrar.setTypeface(typeFace);
-        
-        //seteamos el evento OnClick del botón btnIngresar
-        Button btnIngresar = (Button) findViewById(R.id.btnIngresar);
-        
-        btnIngresar.setOnClickListener(new OnClickListener() 
-        {   public void onClick(View v) 
-            {   
-                Intent intent = new Intent(getApplicationContext(), ModUsuarioActivity.class);
-                    startActivity(intent);      
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//la aplicación espera 3 segundos antes de comenzar, mostrando un splash
+		//En este activity se debería lanzar la inicialización de la aplicación
+		final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                	Intent intent = new Intent(getApplicationContext(), LoginUsuarioActivity.class);
+                    startActivity(intent);
+                    finish();
             }
-        });  
-        
-        
-        /////////////////////
-        Button btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
-        
-        btnRegistrar.setOnClickListener(new OnClickListener() 
-        {   public void onClick(View v) 
-            {   
-	        	/*HttpPost httpPost = new HttpPost(String.format(URL_REGISTRAR_USR,
-	        			edtTextAlias.getText().toString(),
-	        			edtTextCorreo.getText().toString(),
-	        			edtTextContrasena.getText().toString()));*/
-	        	
-	        	HttpGet httpget = new HttpGet(String.format(URL_REGISTRAR_USR,
-	        			edtTextAlias.getText().toString(),
-	        			edtTextCorreo.getText().toString(),
-	        			edtTextContrasena.getText().toString()));
-	        	
-	        	HttpResponse response;
-	        	HttpClient httpClient = new DefaultHttpClient();
-	        	
-        		try {
-					response = httpClient.execute(httpget);
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-            }
-        });      
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+        }, 3000);		
+	}
 }
